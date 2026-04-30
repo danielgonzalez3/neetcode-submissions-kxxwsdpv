922b3256226e20ef81646a596d39819825363adf
Daniel Gonzalez <44592329+danielgonzalez3@users.noreply.github.com>
2026-04-29 21:54:25 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        min_n = 101
+        for n in prices:
+            min_n = min(min_n, n)
+            delta = n - min_n
+            profit = max(profit, delta)
+        return profit
